33f4a5da196ce33aeb99d86b17ba1cf9a73dbc08
sauravmunde <souravmunde2001@gmail.com>
2025-06-23 16:00:55 +0530

    first commit

--- TrangularStar_Pattern.java
+++ TrangularStar_Pattern.java
@@ -2,17 +2,26 @@ public class TrangularStar_Pattern {
 
     public static void main(String[] args) {
 
+        /*
+         * Problem statement
+         * Print the following pattern for the given N number of rows.
+         * 
+         * Pattern for N = 4
+         * 1
+         * 22
+         * 333
+         * 4444
+         */
+
         int n = 4;
 
         for (int i = 1; i <= n; i++) {
 
             for (int j = 1; j <= i; j++) {
 
-                System.out.print("*");
+                System.out.print(i);
             }
             System.out.println();
         }
-
     }
-
 }
