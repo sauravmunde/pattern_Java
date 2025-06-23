3102d121a220e96244d43797ac686e6ba9fe2008
sauravmunde <souravmunde2001@gmail.com>
2025-06-23 16:07:14 +0530

    first commit

---
+++ triangularNum_StarPattern.java
@@ -0,0 +1,17 @@
+public class triangularNum_StarPattern {
+
+    public static void main(String[] args) {
+
+        int n = 4;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= i; j++) {
+
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+
+}
